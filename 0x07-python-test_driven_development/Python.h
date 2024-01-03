f5655cde44f2841e5b737390c465fa246f929754
BatiYounes <younesbati007@gmail.com>
2024-01-03 05:44:09 -0800

    adding the advanced tasks

---
+++ 0x07-python-test_driven_development/Python.h
@@ -0,0 +1,17 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for project
+ */
+
+void print_python_string(PyObject *p);
+
+#endif /* LISTS_H */
