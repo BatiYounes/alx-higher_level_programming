d50bf669fab0057be7d7a6552bddfe9b40fa13d6
BatiYounes <younesbati007@gmail.com>
2024-03-15 03:58:26 -0700

    doing the task of 12-JavaSCRIPT WARM UP

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,10 +2,10 @@
 
 const x = parseInt(process.argv[2]);
 
-if (isNaN(x) || x <= 0 || !Number.isInteger(x)) {
+if (isNaN(x) || !Number.isInteger(x)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < x; i++) {
+  for (let i = 0; i < Math.abs(x); i++) {
     console.log('C is fun');
   }
 }
