3d4a5355112677cb214910006602f3e4a9fd62f6
BatiYounes <younesbati007@gmail.com>
2024-03-15 03:46:26 -0700

    doing the task of 12-JavaSCRIPT WARM UP

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 
-const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
-let output = '';
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (const line of lines) {
-  output += line + '\n';
+  console.log(line);
 }
-
-console.log(output.trim());
