07314474e421aa8aa9915f879c199bb50497a10e
BatiYounes <younesbati007@gmail.com>
2024-03-21 04:47:56 -0700

    doing the task of 12-JavaSCRIPT WARM UP

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-function secondBiggest (arr) {
+function secondBiggest(arr) {
   if (arr.length <= 2) {
     return 0;
   }
-  arr.sort((a, b) => b - a);
-  return arr[1];
+  const sortedArr = arr.sort((a, b) => b - a);
+  return sortedArr[1];
 }
 
 const args = process.argv.slice(2).map(Number);
