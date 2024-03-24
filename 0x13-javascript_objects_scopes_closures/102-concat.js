e585cedc73da234bea91b97862386daaa3b5c20b
BatiYounes <Younesbati007@gmail.com>
2024-03-24 10:01:44 -0700

    doing the task of 0x13 - Java script squares and rectangles

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,19 +2,24 @@
 
 const fs = require('fs');
 
+// Check if the correct number of arguments is provided
 if (process.argv.length !== 5) {
   console.error('Usage: ./102-concat.js fileA fileB fileC');
   process.exit(1);
 }
 
-const [, , fileA, fileB, fileC] = process.argv;
+// Extract file paths from command-line arguments
+const [,, fileA, fileB, fileC] = process.argv;
 
 try {
+  // Read the contents of fileA and fileB
   const contentA = fs.readFileSync(fileA, 'utf8').trim();
   const contentB = fs.readFileSync(fileB, 'utf8').trim();
 
-  const concatenatedContent = `${contentA}\n${contentB}`;
+  // Concatenate the contents with a newline in between
+  const concatenatedContent = `${contentA}\n${contentB}\n`;
 
+  // Write the concatenated content to fileC
   fs.writeFileSync(fileC, concatenatedContent);
 
   console.log(`Concatenated ${fileA} and ${fileB} into ${fileC}`);
