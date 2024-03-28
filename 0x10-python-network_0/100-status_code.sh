67f668421b7cb1c2266d4e0c651b458aa6a4082c
BatiYounes <younesbati007@gmail.com>
2024-03-28 05:26:18 -0700

    doing the task of python netwsorking #0

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -2,12 +2,5 @@
 
 # Bash script to send a request to a URL and display only the status code of the response
 
-# Check if URL argument is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
 # Send request and extract status code
-status_code=$(curl -s -o /dev/null -w "%{http_code}" "$1")
-echo "$status_code"
+curl -s -o /dev/null -w "%{http_code}" "$1"
