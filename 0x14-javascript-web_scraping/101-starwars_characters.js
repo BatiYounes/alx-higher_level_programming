b63b506741fc98f34e4fd21352646b07f78d6a17
BatiYounes <Younesbati007@gmail.com>
2024-04-23 16:03:12 -0700

    doing task for 0X14-java script webscraping

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+// Script to fetch and print all character names from a given Star Wars movie using the Star Wars API
+
+const request = require('request');
+
+const movieId = process.argv[2];
+
+if (!movieId || isNaN(movieId)) {
+  console.log('Please provide a valid movie ID.');
+  process.exit(1);
+}
+
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Error:', body);
+    return;
+  }
+
+  const filmData = JSON.parse(body);
+  const charactersUrls = filmData.characters;
+
+  charactersUrls.forEach(url => {
+    request(url, (error, response, body) => {
+      if (error) {
+        console.error('Error:', error);
+        return;
+      }
+
+      if (response.statusCode !== 200) {
+        console.error('Error:', body);
+        return;
+      }
+
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+    });
+  });
+});
