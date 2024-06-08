0232f0dcbb4b03858480ae217faaa61048d1efee
Younes <121891057+BatiYounes@users.noreply.github.com>
2024-06-08 19:03:46 +0100

    Delete 0x10-python-network_0 directory

--- 0x10-python-network_0/100-status_code.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Bash script to send a request to a URL and display only the status code of the response
-
-# Send request and extract status code
-curl -s -o /dev/null -w "%{http_code}" "$1"
