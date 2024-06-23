a1398550ca074fd2462e4bbeb25c357619d352a3
BATIYounes <Younesbati007@gmail.com>
2024-06-23 16:51:51 -0700

    doing the task of python of objects relational

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""Lists states"""
+"""Lists cities"""
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from relationship_state import Base
 
 class City(Base):
-    """Class representing the states table"""
+    """Class representing the cities table"""
     __tablename__ = 'cities'
 
-    id = Column(Integer, nullable=False, primary_key=True,
-                autoincrement=True, unique=True)
+    id = Column(Integer, nullable=False, primary_key=True, autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
