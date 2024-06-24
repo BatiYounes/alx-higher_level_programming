8f15cbda39e3b3da7d15834a9e458a48a614bf35
BATIYounes <Younesbati007@gmail.com>
2024-06-23 17:03:42 -0700

    doing the task of python of objects relational

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -7,17 +7,16 @@ from sqlalchemy.orm import relationship, backref
 
 Base = declarative_base()
 
-
 class State(Base):
     """Class representing the states table"""
     __tablename__ = 'states'
 
-    id = Column(Integer, nullable=False, primary_key=True,
-                autoincrement=True, unique=True)
+    id = Column(Integer, nullable=False, primary_key=True, autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
 
     cities = relationship(
         "City",
         cascade="all, delete-orphan",
         backref=backref("state", cascade="all"),
-        single_parent=True)
+        single_parent=True
+    )
