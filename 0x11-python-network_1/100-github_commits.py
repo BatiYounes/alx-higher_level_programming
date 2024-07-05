6970749331b7dc10a4544a23f6fae355240d24f5
Younesbati <Younesbati007@gmail.com>
2024-07-05 07:04:53 -0700

    doing the tasks of networking 1

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Script that retrieves and prints the 10 most recent commits
+from a specified GitHub repository by a given owner.
+Uses the GitHub API.
+Uses requests and sys packages.
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+
+    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
+    params = {
+        'per_page': 10,
+    }
+
+    response = requests.get(url, params=params)
+    commits = response.json()
+
+    for commit in commits:
+        sha = commit['sha']
+        author_name = commit['commit']['author']['name']
+        print(f"{sha}: {author_name}")
