0afee3283739cadf7ceb76ea76041f6df4ef99ad
Younesbati <younesbati007@gmail.come>
2024-07-31 06:46:19 -0700

    adding the tasks of 0x14 web scrabing

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const film = JSON.parse(body);
+  const characters = film.characters;
+
+  printCharactersInOrder(characters, 0);
+});
+
+function printCharactersInOrder(characters, index) {
+  if (index >= characters.length) {
+    return;
+  }
+
+  request(characters[index], function (error, response, body) {
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    const character = JSON.parse(body);
+    console.log(character.name);
+    printCharactersInOrder(characters, index + 1);
+  });
+}
